12af93cdcc5cff7446dd032632f4eafce12e3dd2
Omar Mabrouk <omersa2002@gmail.com>
2024-10-07 22:56:12 +0300

    changes

--- script.js
+++ script.js
@@ -201,4 +201,10 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+const menu=document.querySelector(".icon-menu")
+const link=document.querySelector("#style-links")
 
+menu.onclick= ()=>{
+    menu.classList.toggle("fa-x")
+    link.classList.toggle("active")
+}
